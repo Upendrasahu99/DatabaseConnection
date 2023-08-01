52b96c0fad788da12de9f921f38fd701f187ec47
Upendra Sahu <upendrasahu1199@gmail.com>
2023-08-01 23:30:29 +0530

    Insert data to table

---
+++ DatabaseConnection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=LAPTOP-2GBJGBE8\SQLEXPRESS;Initial Catalog=DatabaseConnection;Integrated Security=True";
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("Connection established succesfully!");
+                int data1 = 4;
+                string data2 = "jjj";
+                string insertQuery = "INSERT INTO EnterData(data1, data2) values('" + data1 + "','" + data2 + "')";
+                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+                insertCommand.ExecuteNonQuery();
+                Console.WriteLine("Data is successfully inserted into table");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
